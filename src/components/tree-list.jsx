d856592f8a5ef47af16fb620efe99d05962cd8be
zuohw1 <zuohongwei1986@163.com>
2018-11-29 10:06:20 +0800

    左树右表参照

--- src/components/tree-list.jsx
+++ src/components/tree-list.jsx
@@ -1,151 +1,187 @@
-import React, { Component } from 'react';
-import '../layout/components/assets/styles/main.less';
-import axios from 'axios';
-import { Tree, Table, Button } from 'antd';
-const DirectoryTree = Tree.DirectoryTree;
-const TreeNode = Tree.TreeNode;
-const columns = [{
-  title: 'Code',
-  dataIndex: 'code',
-}, {
-  title: 'Number',
-  dataIndex: 'number',
-}, {
-  title: 'Name',
-  dataIndex: 'name',
-}];
-class TreeList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            userName: '',
-            selectedRowKeys: [],
-            data:[],
-            arr:[],
-            res:[],
-        }
-    }
-    onSelectChange = (selectedRowKeys) => {
-        console.log('selectedRowKeys changed: ', selectedRowKeys);
-        this.setState({ selectedRowKeys });
-    }
-  	showConTrli=() => {
-  		this.refs.covTrli.style.display = "block";
-  		this.refs.conTrli.style.display = "block";
-        this.setState({
-            data: this.state.arr.slice(0,18)
-        });
-  	}
-  	closeConTrli=() => {
-  	    this.refs.covTrli.style.display = "none";
-  	    this.refs.conTrli.style.display = "none";
-  	}
-    onSelect = (selectedKeys,selectedNodes) => {
-        if(selectedKeys === "0-0"){
-            this.setState({
-              data: this.state.arr.slice(0,18)
-            });
-        }else if(selectedKeys === "0-0-0"){
-            this.setState({
-              data: this.state.arr.slice(18,36)
-            });
-        }else if(selectedKeys === "0-0-1"){
-            this.setState({
-              data: this.state.arr.slice(36,47)
-            });
-        }else if(selectedKeys === "0-1"){
-            this.setState({
-              data: this.state.arr.slice(47,74)
-            });
-        }else if(selectedKeys === "0-1-0"){
-            this.setState({
-              data: this.state.arr.slice(74,96)
-            });
-        }else if(selectedKeys === "0-1-1"){
-            this.setState({
-              data: this.state.arr.slice(96,110)
-            });
-        }
-    }
-    onExpand = () => {
-        console.log('Trigger Expand');
-    }
-    rowClick=(record, index) =>{
-        this.setState({
-              userName: record.name
-            })
-        this.setState({
-          key: record.key
-        })
-        var aLi = document.getElementsByClassName("ant-table-row-level-0");
-        for(var i = 0; i < aLi.length; i ++){
-          aLi[i].classList.remove("row-selected");
-          if(i === index){
-            aLi[i].className += ' row-selected';
-          }
-        }
-    }
-    enterData=() => {
-        this.refs.covList.style.display = "none";
-        this.refs.conList.style.display = "none";
-        console.log(this.state.userName)
-      console.log(this.state.data)
-    }
-    componentDidMount() {
-        axios.get('https://www.easy-mock.com/mock/5b9f51bc8b5cc40f1f28a324/example/demolist')
-        .then( (response)=> this.setState({ arr : response.data }) )
-        .catch(function (error) {
-            console.log(error);
-        })
-    }
-    render() {
-        const { selectedRowKeys } = this.state;
-        const rowSelection = {
-          selectedRowKeys,
-          onChange: this.onSelectChange,
-          hideDefaultSelections: true,
-          onSelection: this.onSelection,
-        }
-        return (
-            <div className="Treelist">
-                <div id="covTrli"  ref="covTrli"></div>
-			    <div id = "conTrli" ref="conTrli">
-		            <div className="trliHeader">左树右表</div>
-                    <div className="trliContent">
-                        <div className="trliContentLeft">
-                            <DirectoryTree
-                                multiple
-                                defaultExpandAll
-                                onSelect={this.onSelect}
-                                onExpand={this.onExpand}
-                            >
-                                <TreeNode title="parent 0" key="0-0">
-                                  <TreeNode title="leaf 0-0" key="0-0-0" isLeaf />
-                                  <TreeNode title="leaf 0-1" key="0-0-1" isLeaf />
-                                </TreeNode>
-                                <TreeNode title="parent 1" key="0-1">
-                                  <TreeNode title="leaf 1-0" key="0-1-0" isLeaf />
-                                  <TreeNode title="leaf 1-1" key="0-1-1" isLeaf />
-                                </TreeNode>
-                            </DirectoryTree>
-                        </div>
-                        <div className="trliContentRight">
-                            <Table
-                                rowSelection={rowSelection}
-                                columns={columns}
-                                dataSource={this.state.data}
-                                onRowClick={this.rowClick}
-                            />
-                        </div>
-                    </div>
-                    <div className="trliFooter">
-                      <Button type="danger" onClick = {this.closeConTrli}  className="shadeButton">关闭</Button>
-                      <Button type="primary" onClick = {this.enterData} className="shadeButton">确定</Button>
-                    </div>
-        			    </div>
-                  <Button type="primary" onClick = {this.showConTrli}  className="openTree">左树右表</Button>
-            </div>
-        );
-    }
+import React from 'react';
+import {
+  Tree, Table, Row, Col, Input, Pagination,
+} from 'antd';
+import request from '../utils/request';
+
+const { Search } = Input;
+
+/**
+ * 左树右表
+ * <TreeList
+ treeId={37838}
+ treeUrl={treeUrl}
+ columns={refColumns}
+ refUrl={refUrl}
+ refCodes={refCodes}
+ refSelectData={refSelectData}
+ onConfirm={onConfirm}
+ />
+ const onConfirm = () => {//双击表格数据后回写值与关闭model
+    const data = { ...record, ...refSelectData };
+    setRefSelectData(data, false);
+  };
+
+ const treeUrl = 'org/allData?id=';//树url
+ const refCodes = [{ code: 'DOC_CODE', refcode: 'docCode' }];//表格与界面字段对应
+ const refColumns = [{//表格显示字段
+    title: '文件名称和文号',
+    dataIndex: 'docCode',
+    key: 'docCode',
+    align: 'center',
+    width: 100,
+  }, {
+    title: '文件拟稿人',
+    dataIndex: 'docVerifier',
+    key: 'docVerifier',
+    align: 'center',
+    width: 80,
+  }];
+ const refUrl = 'orgHeaderBatch/list';//表格url
+ const refSelectData = {};//表格选中数据
+ */
+class TreeList extends React.PureComponent {
+  state = {
+    treeData: [],
+    search: {
+      pageNumber: 1,
+      pageSize: 10,
+      name: '',
+    },
+    refData: {
+      current: 1, pages: 0, records: Array(0), size: 10, total: 0,
+    },
+    tableLoading: false,
+    selectedKey: '',
+  };
+
+  /**
+   *第一次渲染后调用
+   * @returns {Promise<void>}
+   */
+  async componentDidMount() {
+    const { treeId, treeUrl } = this.props;
+    const treeData = await request.get(`${treeUrl}${treeId}`);
+    this.setState({ treeData });
+  }
+
+  formatTableData = (tableData) => {
+    const num = tableData.current * 10 - 10;
+    const table = tableData.records.map((item, index) => {
+      return { ...item, key: index + 1 + num };
+    });
+    return { ...tableData, records: table };
+  };
+
+  onSearch = (value) => {
+    const { refUrl } = this.props;
+    const { search, selectedKey } = this.state;
+    search.name = value;
+    this.setState(search);
+    this.refreshData(refUrl, search, selectedKey);
+  };
+
+  onChangePage = (pageNumber, pageSize) => {
+    const { refUrl } = this.props;
+    const { search, selectedKey } = this.state;
+    const searchF = { ...search, pageSize, pageNumber };
+    this.refreshData(refUrl, searchF, selectedKey);
+  };
+
+  onChangePageSize = (current, size) => {
+    const { refUrl } = this.props;
+    const { search, selectedKey } = this.state;
+    const searchF = { ...search, pageSize: size, pageNumber: current };
+    this.refreshData(refUrl, searchF, selectedKey);
+  };
+
+  refreshData = (refUrl, search, selectedKey) => {
+    this.setState({ tableLoading: true, selectedKey });
+    return new Promise(async (resolve) => {
+      let url = `${refUrl}?pageNumber=${search.pageNumber}&pageSize=${search.pageSize}`;
+      if (search.name && search.name !== '') {
+        url += `&name=${search.name}`;
+      }
+      url += `&orgid=${selectedKey}`;
+      console.log(url);
+      const tableData = await request.get(url);
+      const formatTable = this.formatTableData(tableData);
+      this.setState({ refData: formatTable, tableLoading: false });
+      resolve();
+    });
+  };
+
+  treeSelect = (selectedKeys) => {
+    const { refUrl } = this.props;
+    const { search } = this.state;
+    this.refreshData(refUrl, search, selectedKeys[0]);
+  }
+
+  render() {
+    const {
+      columns, placeholder, refCodes, refSelectData, onConfirm,
+    } = this.props;
+    const {
+      refData, tableLoading, treeData,
+    } = this.state;
+    const {
+      current, size, total, records,
+    } = refData;
+
+    return (
+      <div className="setting-treelist">
+        <Row type="flex" justify="space-around">
+          <Col span={7}>
+            <Tree
+              onSelect={this.treeSelect}
+              treeData={treeData}
+              style={{ height: 360, overflow: 'scroll' }}
+            />
+          </Col>
+          <Col span={1} className="tree-border" />
+          <Col span={16}>
+            <Search
+              style={{ marginBottom: 8 }}
+              placeholder={placeholder}
+              onSearch={this.onSearch}
+            />
+            <Table
+              columns={columns}
+              dataSource={records}
+              size="small"
+              pagination={false}
+              bordered
+              scroll={{ y: 300 }}
+              loading={tableLoading}
+              onRow={(record) => {
+                return {
+                  /* 双击行事件，执行确定动作，回写选中数据，关闭modal框 */
+                  onDoubleClick: () => {
+                    refCodes.map((item) => {
+                      /* eslint-disable no-param-reassign,no-return-assign */
+                      return refSelectData[item.code] = record[item.refcode];
+                    });
+                    onConfirm();
+                  },
+                };
+              }}
+            />
+            <Pagination
+              size="small"
+              showQuickJumper
+              current={current}
+              total={total}
+              pageSize={size}
+              onChange={this.onChangePage}
+              onShowSizeChange={this.onChangePageSize}
+              showTotal={tota => `共 ${tota} 条`}
+              showSizeChanger
+              style={{ marginTop: 10, float: 'right' }}
+            />
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
 export default TreeList;
