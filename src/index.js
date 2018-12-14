b7072dfcf36e0350e164c6e9e22c572a60b25ffe
zuohw1 <zuohongwei1986@163.com>
2018-12-14 15:15:50 +0800

    添加日期多语

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import React from 'react';
 import { LocaleProvider, message } from 'antd';
 import createLoading from 'dva-loading';
 import { createLogger } from 'redux-logger';
-import zhCN from 'antd/lib/locale-provider/zh_CN';
 import { Router, Route, Switch } from 'dva/router';
+import zhCN from 'antd/lib/locale-provider/zh_CN';
+import moment from 'moment';
 import Layout from './layout/containers';
 import Configure from './env.config';
 import customizedHistory from './utils/history';
 import { debounce } from './utils/helper';
+import 'moment/locale/zh-cn';
+
+moment.locale('zh-cn');
 
 const app = dva({
   history: customizedHistory,
