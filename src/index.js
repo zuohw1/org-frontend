a08bd63c59ca7fc53c1090fa2c2ee7573b92292e
zuohw1 <zuohongwei1986@163.com>
2018-12-18 15:13:37 +0800

    添加dva中间件

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import Configure from './env.config';
 import customizedHistory from './utils/history';
 import { debounce } from './utils/helper';
 import 'moment/locale/zh-cn';
+import frameHook from './utils/framehook';
 
 moment.locale('zh-cn');
 
@@ -21,6 +22,7 @@ const app = dva({
   }, 1.5),
 });
 app.use(createLoading());
+app.use(frameHook());
 if (Configure.debug) {
   app.use({
     onAction: createLogger(),
